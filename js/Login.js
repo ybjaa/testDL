cfdd691075acda25a9160d7ccaa690c0fa8e61f7
ybjaa <ybjaa@163.com>
2017-03-02 23:49:01 +0800

    登录页面完成

---
+++ js/Login.js
@@ -0,0 +1,9 @@
+/**
+ * Created by ybjaa on 2017/3/2.
+ */
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById("loginSubmit").addEventListener("click",function (event) {
+        event.preventDefault();
+    })
+})
