b3984f0d619525bd8083a6a0969035398c2a4f4a
ybjaa <ybjaa@163.com>
2017-03-20 10:36:04 +0800

    加入测试代码

---
+++ php/TestCalss.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ybjaa
+ * Date: 2017/3/14
+ * Time: 11:04
+ */
+
+namespace test_ybjaa;
+
+
+class TestCalss
+{
+
+}
---
+++ php/test.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <body>
+
+        <?php
+        echo "我的第一段 PHP 脚本！";
+        phpinfo();
+        $aa="aa";
+        echo $aa;
+
+        ?>
+
+    </body>
+</html>
